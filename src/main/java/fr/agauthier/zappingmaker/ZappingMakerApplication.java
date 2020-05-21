3ee93fcec079376b072f4769f235fcbf286859c3
Antoine Gauthier <agauthier@doubletrade.com>
2020-05-21 17:24:28 +0200

    First commit

---
+++ src/main/java/fr/agauthier/zappingmaker/ZappingMakerApplication.java
@@ -0,0 +1,13 @@
+package fr.agauthier.zappingmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZappingMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZappingMakerApplication.class, args);
+	}
+
+}
